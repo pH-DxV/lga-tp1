ac031fe6bd06f4ef182d9c878cda1d1a5b368894
pH-DxV <raphaelempriori@gmail.com>
2026-04-11 05:38:19 -0300

    Commit final 11/04

--- frontend/src/app/components/estados/estado-list/estado-list.ts
+++ frontend/src/app/components/estados/estado-list/estado-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EstadoService } from '../../../services/estado.service'
 import { Estado } from '../../../models/estado'
 
@@ -9,6 +9,19 @@ import { Estado } from '../../../models/estado'
   templateUrl: './estado-list.html',
   styleUrls: ['./estado-list.css'],
 })
-export class EstadoListComponent {
+export class EstadoListComponent implements OnInit{
+
+    // 👉 1. AQUI ficam as variáveis da classe
+  estados: Estado[] = []
+
+  // 👉 2. AQUI fica o constructor
+  constructor(private estadoService: EstadoService) {}
+
+  // 👉 3. AQUI fica o ngOnInit
+  ngOnInit(): void {
+    this.estadoService.findAll().subscribe(data => {
+      this.estados = data
+    })
+  }
 
 }
