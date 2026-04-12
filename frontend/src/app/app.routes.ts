7078c06c6e8889df9f4cb63789732906ed7a867d
pH-DxV <raphaelempriori@gmail.com>
2026-04-12 02:27:14 -0300

    Still blanket at /estados

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EstadoListComponent } from './components/estados/estado-list/estado-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'estados', component: EstadoListComponent }
+];
--- frontend/src/app/components/estados/estado-list/estado-list.ts
+++ frontend/src/app/components/estados/estado-list/estado-list.ts
@@ -1,27 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { EstadoService } from '../../../services/estado.service'
-import { Estado } from '../../../models/estado'
+import { CommonModule } from '@angular/common'; // 👈 ADD AQUI
+import { EstadoService } from '../../../services/estado.service';
+import { Estado } from '../../../models/estado';
 
 @Component({
   selector: 'app-estado-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // 👈 E AQUI
   templateUrl: './estado-list.html',
   styleUrls: ['./estado-list.css'],
 })
-export class EstadoListComponent implements OnInit{
+export class EstadoListComponent implements OnInit {
 
-    // 👉 1. AQUI ficam as variáveis da classe
-  estados: Estado[] = []
+  estados: Estado[] = [];
 
-  // 👉 2. AQUI fica o constructor
   constructor(private estadoService: EstadoService) {}
 
-  // 👉 3. AQUI fica o ngOnInit
   ngOnInit(): void {
     this.estadoService.findAll().subscribe(data => {
-      this.estados = data
-    })
+      this.estados = data;
+    });
   }
-
 }
